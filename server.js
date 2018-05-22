47eb6f7a285929a0c12e1ccb7c9506042294ffed
Alexei <alexis.cabrera@gmail.com>
2018-05-22 19:57:34 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'aleXis cabrERA',
+        anyo: new Date().getFullYear()
+    });
+
+});
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+        anyo: new Date().getFullYear()
+    });
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
